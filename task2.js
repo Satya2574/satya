7c3e1f2bd86e2a13b40c7fd2bd6e9bdd9536ecb5
SATYANARAYANA REDDY <satyanarayanareddy@SATYANARAYANAs-MacBook-Air.local>
2021-12-07 13:52:45 +0530

    made a change

--- task2.js
+++ task2.js
@@ -1,18 +1,2 @@
 console.log("hello world");
 nsole.log("hello world");
-nsole.log("hello world");
-nsole.log("hello world");
-nsole.log("hello world");
-nsole.log("hello world");
-nsole.log("hello world");
-nsole.log("hello world");
-nsole.log("hello world");
-
-nsole.log("hello world");
-nsole.log("hello world");
-nsole.log("hello world");
-nsole.log("hello world");
-nsole.log("hello world");
-nsole.log("hello world");
-nsole.log("hello world");
-nsole.log("hello world");
